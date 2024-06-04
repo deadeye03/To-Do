57af3752981bd2633c9c1f4dad100dcb970d572a
deadeye03 <yakshitkr@gmail.com>
2024-06-04 15:12:23 +0530

    add compression package

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const bodyParser = require('body-parser');
 const express=require('express');
 const routing=require('./todoRoute');
 const dotenv=require('dotenv');
-
+const compression=require('compression')
 dotenv.config({path:'./config.env'})
 const app=express();
 app.set('view engine', 'ejs') //for rendering my html page
 app.use(express.static('public')) // serving static file
 app.use(express.json());
+app.use(compression());
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
