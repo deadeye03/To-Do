2a94d3616195fdb865afd7c7da3b409f3743aba0
deadeye03 <yakshitkr@gmail.com>
2024-06-05 23:53:46 +0530

    change env config file

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express=require('express');
 const routing=require('./todoRoute');
 const dotenv=require('dotenv');
 const compression=require('compression')
-dotenv.config({path:'./config.env'})
 const app=express();
+dotenv.config({path:'./config.env'})  
 app.set('view engine', 'ejs') //for rendering my html page
 app.use(express.static('public')) // serving static file
 app.use(express.json());
