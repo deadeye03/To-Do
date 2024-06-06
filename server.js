dd200dc2c1f9a5053c3911bf5207017d25692fd5
deadeye03 <yakshitkr@gmail.com>
2024-06-06 14:25:54 +0530

    Addin path.join for static

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express=require('express');
 const routing=require('./todoRoute');
 const dotenv=require('dotenv');
 const compression=require('compression')
+const path=require('path')
 const app=express();
 dotenv.config({path:'./config.env'})  
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views')) // serving static file
 app.set('view engine', 'ejs') //for rendering my html page
-app.use(express.static('public')) // serving static file
 app.use(express.json());
 app.use(compression());
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
